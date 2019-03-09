b428c89975dd5f7ca71c97f1f28e4e79c7284a57
kimlabtoy <48376994+kimlabtoy@users.noreply.github.com>
2019-03-09 15:38:11 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# microbot-
+# tools
+
+測試microbit積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+tools=github:kimlabtoy/microbot
+```
---
+++ main.ts
@@ -0,0 +1,28 @@
+//% weight=0 color=#3CB371 icon="\uf6b0" block="KimlabRobot"
+namespace KimlabRobot {
+    /**
+    * 計算長方形面積，並回傳
+    */
+	
+	/*
+    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function areaOfRectangle(length: number, width:number): number {
+        return length*width
+    }
+	*/
+    /**
+    * 計算長方形面積，不回傳，只顯示在LED
+    */
+    //% blockId="LedTest" block="Smile"
+    //% blockGap=2 weight=1
+    export function LedTest(): void {
+        basic.showLeds(`
+    . # . # .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+    }
+}
