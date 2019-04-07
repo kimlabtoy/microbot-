138c5b8b5c0c07880c1deefbbaaa9fa48c8ca613
kimlabtoy <48376994+kimlabtoy@users.noreply.github.com>
2019-04-07 13:46:51 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -126,6 +126,7 @@ namespace KimlabRobot {
         buf[4] = (pulselen>>8) & 0xff;
         pins.i2cWriteBuffer(IIC_ADDRESS, buf);
     }
+	
 	//% blockId=OTTO_Servo
     //% block="OTTO_RHAND %RHAND| OTTO_LHAND %LHAND| OTTO_RLEG %RLEG| OTTO_LLEG %LLEG| OTTO_RFOOT %RFOOT| OTTO_LFOOT %LFOOT "
     //% RHand.min=0 RHAND.max=180 LHand.min=0 LHAND.max=180 RLEG.min=0 RLEG.max=180 LLEG.min=0 LLEG.max=180 RFOOT.min=0 RFOOT.max=180 LFOOT.min=0 LFOOT.max=180
@@ -133,5 +134,6 @@ namespace KimlabRobot {
         
         
     }
+	
     
 }
