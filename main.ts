05cc67acf7c686a72ca881afb30899dd166201aa
kimlabtoy <48376994+kimlabtoy@users.noreply.github.com>
2019-04-07 13:29:14 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -33,22 +33,22 @@ namespace KimlabRobot {
     const LED0_ON_L = 0x06
     
     export enum ServoNum {
-        S0 = 0,
-        S1 = 1,
-        S2 = 2,
-        S3 = 3,
-        S4 = 4,
-        S5 = 5,
-        S6 = 6,
-        S7 = 7,
-        S8 = 8,
-        S9 = 9,
-        S10 = 10,
-        S11 = 11,
-        S12 = 12,
-        S13 = 13,
-        S14 = 14,
-        S15 = 15,
+        S1 = 0,
+        S2 = 1,
+        S3 = 2,
+        S4 = 3,
+        S5 = 4,
+        S6 = 5,
+        S7 = 6,
+        S8 = 7,
+        S9 = 8,
+        S10 = 9,
+        S11 = 10,
+        S12 = 11,
+        S13 = 12,
+        S14 = 13,
+        S15 = 14,
+        S16 = 15,
         
     }
 
@@ -126,5 +126,36 @@ namespace KimlabRobot {
         buf[4] = (pulselen>>8) & 0xff;
         pins.i2cWriteBuffer(IIC_ADDRESS, buf);
     }
+	
+	//% blockId=OTTO_Servo
+    //% block="Servo channel %channel|degrees %degree\n Servo channel %channel|degrees %degree\n Servo channel %channel|degrees %degree\n Servo channel %channel|degrees %degree\n Servo channel %channel|degrees %degree\n "
+    //% degree.min=0 degree.max=180
+	export function Servo(channel: ServoNum, degree: number): void {
+        
+        if(!initialized){
+			init()
+		}
+		// 50hz: 20,000 us
+        //let servo_timing = (degree*1800/180+600) // 0.55 ~ 2.4
+        //let pulselen = servo_timing*4096/20000
+        //normal 0.5ms~2.4ms
+        //SG90 0.5ms~2.0ms
+
+        let pulselen = servo_map(degree, 0, 180, 112, 491);
+        //let pulselen = servo_map(degree, 0, 180, servomin, servomax);
+        
+        
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = 0;
+        buf[2] = (0>>8);
+        buf[3] = pulselen & 0xff;
+        buf[4] = (pulselen>>8) & 0xff;
+        pins.i2cWriteBuffer(IIC_ADDRESS, buf);
+    }
+	
     
 }
